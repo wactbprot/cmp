79c632b7b5dfdc54a32bc719903a9ee91de2011d
Wact B. Prot <wactbprot@gmail.com>
2019-04-19 13:37:48 +0200

    ..

--- src/cmp/st.clj
+++ src/cmp/st.clj
@@ -6,22 +6,9 @@
 (def conn {:pool {} :spec {:host "127.0.0.1" :port 6379}})
 (defmacro wcar* [& body] `(car/wcar conn ~@body))
 
-(defn distrib-exchange [main-path {exchange :Exchange}]
-  (map (fn [e]
-         (let [{elem-path :path value :value} (utils/get-key-and-map e)]
-           (def k (utils/gen-st-key [main-path "exchange" elem-path]))
-           (def v (utils/gen-st-value value))
-           (wcar* (car/set k v))))
-       exchange))
-
 (defn get-keys [path]
   (def k (utils/gen-st-key [path "*"]))
   (wcar* (car/keys k)))
-
-(defn distrib [{id :_id rev :_rev mp :Mp}]
-  (def p (utils/extr-main-path id))
-  (clear-exchange p)
-  (distrib-exchange p mp))
 
 (defn del-keys [ks]
   (map (fn [k]
@@ -37,3 +24,16 @@
   (def p (utils/gen-st-key [main-path "exchange"]))
   (def k (get-keys p))
   (del-keys k))
+
+(defn distrib-exchange [main-path {exchange :Exchange}]
+  (map (fn [e]
+         (let [{elem-path :path value :value} (utils/get-key-and-map e)]
+           (def k (utils/gen-st-key [main-path "exchange" elem-path]))
+           (def v (utils/gen-st-value value))
+           (wcar* (car/set k v))))
+       exchange))
+
+(defn distrib [{id :_id rev :_rev mp :Mp}]
+  (def p (utils/extr-main-path id))
+  (clear-exchange p)
+  (distrib-exchange p mp))
