f15c46113048bccfe7034e6f5c16789e3f1270b4
Wact B. Prot <wactbprot@gmail.com>
2019-05-19 20:07:50 +0200

    polling ok

---
+++ src/cmp/poll.clj
@@ -0,0 +1,32 @@
+(ns cmp.poll
+  ^{:author "wactbprot"
+    :doc "Builds up the short term memory with given the mp-definition."}
+  (:require [taoensso.timbre :as log]
+            [cmp.st :as st]
+            [cmp.utils :as u])
+  (:gen-class))
+
+(log/set-level! :debug)
+
+(def heartbeat 500)
+(def future-calls
+  (atom {}))
+
+(defn start-cont-mon
+  [p i]
+  (let [ctrl-path (u/gen-key [p "container" i "ctrl"])]
+    (future
+      (while true
+        (do
+          (Thread/sleep heartbeat)
+          (println (st/get-val ctrl-path)))))))
+
+(defn register [p f]
+  (swap!
+   future-calls
+   assoc p f))
+
+
+(defn stop-cont-mon
+  [p]
+  (future-cancel (@future-calls p)))
