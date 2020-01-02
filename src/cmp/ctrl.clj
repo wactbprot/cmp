61622256b2e706ab4bb6b003c859643e92b92b03
Wact B. Prot <wactbprot@gmail.com>
2020-01-02 17:38:16 +0100

    next: register must be sensible to struct and  no-idx.

--- src/cmp/ctrl.clj
+++ src/cmp/ctrl.clj
@@ -12,28 +12,30 @@
 ;; ctrl-dispatch!, start, stop
 ;;------------------------------
 (defn stop
+  "De-registers a listener for the `ctrl` interface of the `mp-id`."
   [mp-id]
   (reg/de-register! mp-id "ctrl"))
 
-(defn ctrl-dispatch!
+(defn dispatch!
   "Dispatches on the ctrl path `p`"
   [p]
   (let [mp-id (u/key->mp-name p)
         cmd (->> p
-                  (st/key->val)
-                  (u/get-next-ctrl))]
-    (timbre/debug "dispatch for key: " p " and cmd: " cmd)
+                 (st/key->val)
+                 (u/get-next-ctrl))]
     (cond
-      (= cmd "run") (state/start p)
-      (= cmd "mon") (state/start p)
+      (= cmd "run")     (state/start p)
+      (= cmd "mon")     (state/start p)
       (= cmd "suspend") (state/stop p)
-      (= cmd "stop") (do
-                      (state/stop p)
-                      (stop mp-id))
+      (= cmd "stop")    (do
+                          (state/stop p)
+                          (stop mp-id))
       :default (timbre/debug "dispatch default branch for key: " p))))
 
 (defn start
+  "Registers a listener for the `ctrl` interface of the `mp-id`.
+  The [[dispatch]] function becomes the `callback`." 
   [mp-id]
   (reg/register! mp-id "ctrl" (fn [msg]
-                            (ctrl-dispatch! (st/msg->key msg)))))
+                            (dispatch! (st/msg->key msg)))))
 
