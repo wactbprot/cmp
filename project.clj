b4126da2e5079a8cb760da6b492bf1329f3c5de1
Wact B. Prot <wactbprot@gmail.com>
2020-01-03 14:28:20 +0100

    .....

--- project.clj
+++ project.clj
@@ -20,7 +20,7 @@
   :plugins [[lein-codox "0.10.7"]
             [ns-graph "0.1.3"]]
   :codox {:output-path "./docs"
-          :doc-paths ["./docs"]
+          :doc-files ["docs/graph.md", "docs/notes.md"]
           :themes [:rdash]
           :metadata {:doc/format :markdown}
           :source-uri "https://github.com/wactbprot/cmp/blob/master/{filepath}#L{line}"}
