34641506bd6194dd4cbdfb45cead84100538ade6
Wact B. Prot <wactbprot@gmail.com>
2020-04-18 17:05:39 +0200

    ..

--- project.clj
+++ project.clj
@@ -15,15 +15,19 @@
                  [biz.paluch.logging/logstash-gelf "1.12.0"]
                  [ch.qos.logback/logback-classic "1.0.1"]
                  [org.graylog2/gelfclient "1.4.1"]
-                 [codox-theme-rdash "0.1.2"]]
+                 ;;[codox-theme-rdash "0.1.2"]
+                 [clojang/codox-theme "0.2.0-SNAPSHOT"]
+                 ]
   :main ^:skip-aot cmp.core
   :target-path "target/%s"
   :plugins [[lein-cloverage "1.1.2"]
             [lein-codox "0.10.7"]
             [ns-graph "0.1.3"]
             [jonase/eastwood "0.3.7"]]
-  ;:cloverage {:output "./coverage"}
-  :codox {:themes [:rdash]
+  :cloverage {:low-watermark 30
+             :high-watermark 60}
+  ;;  :codox {:themes [:rdash]
+  :codox {:themes [:clojang]
           :metadata {:doc/format :markdown}
           :source-uri "https://github.com/wactbprot/cmp/blob/master/{filepath}#L{line}"}
   :ns-graph {:name "cmp"
