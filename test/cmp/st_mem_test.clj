a1b4e48abecf6f758f92b873b7f2f672fd7b54d3
Wact B. Prot <wactbprot@gmail.com>
2020-12-13 17:41:50 +0100

    st-mem tests

--- test/cmp/st_mem_test.clj
+++ test/cmp/st_mem_test.clj
@@ -1,5 +1,6 @@
 (ns cmp.st-mem-test
   (:require [clojure.test :refer :all]
+            [cmp.utils :as u]
             [cmp.st-mem :refer :all]))
 
 (def k "test")
@@ -9,4 +10,79 @@
     (let [v "string"]
       (set-val! k v) 
       (is (= v (key->val k))
+          (str "set get " v))
+      (del-key! k)
+      (is (nil? (key->val k))
+          (str "set get " v)))))
+
+(deftest set-get-del-ii
+  (testing "bool"
+    (let [v true]
+      (set-val! k v) 
+      (is (= v (key->val k))
+          (str "set get " v))
+      (del-key! k)
+      (is (nil? (key->val k))
+          (str "set get " v)))))
+
+(deftest set-get-del-iii
+  (testing "number"
+    (let [v 123456789]
+      (set-val! k v) 
+      (is (= v (key->val k))
+          (str "set get " v))
+      (del-key! k)
+      (is (nil? (key->val k))
+          (str "set get " v)))))
+
+(deftest set-get-del-iv
+  (testing "vec"
+    (let [v [1]]
+      (set-val! k v) 
+      (is (= v (key->val k))
+          (str "set get " v))
+      (del-key! k)
+      (is (nil? (key->val k))
+          (str "set get " v)))))
+
+(deftest set-get-del-v
+  (testing "vec"
+    (let [v {:a 1}]
+      (set-val! k v) 
+      (is (= v (key->val k))
+          (str "set get " v))
+      (del-key! k)
+      (is (nil? (key->val k))
+          (str "set get " v)))))
+
+(deftest set-get-del-vi
+  (testing "vec"
+    (let [v '(1)]
+      (set-val! k v) 
+      (is (= v (key->val k))
+          (str "set get " v))
+      (del-key! k)
+      (is (nil? (key->val k))
+          (str "set get " v)))))
+
+
+(deftest set-get-del-vi
+  (testing "vec"
+    (let [v '(1)]
+      (set-same-val! [k] v) 
+      (is (= v (key->val k))
+          (str "set get " v))
+      (del-keys! [k])
+      (is (nil? (key->val k))
+          (str "set get " v)))))
+
+(deftest clear-i
+  (testing "vec"
+    (let [v  1
+          kk (u/vec->key [k k])]
+      (set-val! kk v) 
+      (is (= v (key->val kk))
+          (str "set get " v))
+      (clear! k)
+      (is (nil? (key->val kk))
           (str "set get " v)))))
