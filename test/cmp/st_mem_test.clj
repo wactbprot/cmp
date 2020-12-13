c10387f418efa3640a6a20c731584a6101c2d9c8
Wact B. Prot <wactbprot@gmail.com>
2020-12-13 16:39:01 +0100

    .

--- test/cmp/st_mem_test.clj
+++ test/cmp/st_mem_test.clj
@@ -1,3 +1,12 @@
 (ns cmp.st-mem-test
   (:require [clojure.test :refer :all]
             [cmp.st-mem :refer :all]))
+
+(def k "test")
+
+(deftest set-get-del-i
+  (testing "string"
+    (let [v "string"]
+      (set-val! k v) 
+      (is (= v (key->val k))
+          (str "set get " v)))))
