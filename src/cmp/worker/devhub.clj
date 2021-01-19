13d1d3140f192e4c426f7e84e11e7e380cc75044
Thomas Bock <thomas.bock@ptb.de>
2021-01-19 08:47:03 +0100

    -prescript

--- src/cmp/worker/devhub.clj
+++ src/cmp/worker/devhub.clj
@@ -9,40 +9,18 @@
             [cmp.key-utils :as ku]
             [taoensso.timbre :as log]
             [cmp.worker.pre-script :as ps]))
-
-(defn resolve-pre-script
-  "Checks if the task has a `:PreScript` (name of the script to run)
-  and an `:Input` key. If not, `task` is returned."
-  [task]
-  (let [{script-name :PreScript
-         input       :PreInput
-         state-key   :StateKey} task]
-    (if (string? script-name)
-      (if (map? input)
-        (condp = (keyword script-name)
-          :set_valve_pos  (ps/set-valve-pos task)
-          :get_valve_pos  (ps/get-valve-pos task)
-          :get_switch_pos (ps/get-switch-pos task)
-          (do
-            (log/error "script: " script-name " not implemented")
-            (st/set-state! state-key :error)))
-        task)
-      task)))
-  
+ 
 (defn devhub!
-  "Param is called `pre-task` because some tasks come with a
-  `:PreScript` which has to be executed in order to complete
-  the task (sometimes the `:Value` is computed by the
-  `:PreScript`).
+  "Sends `:Value` to [devhub](https://wactbprot.github.io/devhub/) which
+  resolves `PreScript` and `PostScript`.
   
   ```clojure
    (devhub! {:Action \"TCP\" :Port 23 :Host \"localhost\" :Value \"Hi!\"})
   ```"
-  [pre-task]
-  (let [state-key   (:StateKey pre-task)]
+  [task]
+  (let [state-key (:StateKey task)]
     (st/set-state! state-key :working)
     (let [request-key (ku/key->request-key state-key)
-          task        (resolve-pre-script pre-task)
           json-task   (u/map->json task)
           req         (assoc (cfg/json-post-header (cfg/config)) :body json-task)
           url         (cfg/dev-hub-url (cfg/config))]
