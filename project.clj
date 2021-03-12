f0e220ac0a035cb1fb66ceba46bb2819e99a949c
Thomas Bock <thomas.bock@ptb.de>
2021-03-12 14:52:26 +0100

    +docs

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cmp "0.21.2"
+(defproject cmp "0.21.3"
   :description "A study of an interpreter for measurement 
   program definitions (mpd) written in clojure."
   :url "https://github.com/wactbprot/cmp"
